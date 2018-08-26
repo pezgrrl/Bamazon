93a5487c1a4016009dabb0b602ad0713074bedfe
Jill Morimoto <jillmorimoto@gmail.com>
2018-08-25 22:01:41 -0700

    8

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,7 +12,94 @@ var connection = mysql.createConnection({
     database: "Bamazon"
 });
 
-function validateInput(value) {
+function promptManagerAction() {
+    inquirer.prompt([{
+        type: "list",
+        name: "option",
+        message: "Please select an option:",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+        filter: function (val) {
+            if (val === "View Products for Sale") {
+                return "sale";
+            } else if (val === "View Low Inventory") {
+                return "lowInventory";
+            } else if (val === "Add to Inventory") {
+                return "addInventory";
+            } else if (val === "Add New Product") {
+                return "newProduct";
+            } else {
+                console.log("ERROR: Unsupported operation!");
+                exit(1);
+            }
+        }
+    }]).then(function (input) {
+        if (input.option === "sale") {
+            displayInventory();
+        } else if (input.option === "lowInventory") {
+            displayLowInventory();
+        } else if (input.option === "addInventory") {
+            addInventory();
+        } else if (input.option === "newProduct") {
+            createNewProduct();
+        } else {
+            console.log("ERROR: Unsupported operation!");
+            exit(1);
+        }
+    })
+}
+
+function displayInventory() {
+    queryStore = "SELECT * FROM products";
+    connection.query(queryStore, function (err, data) {
+        if (err) throw err;
+
+        console.log("Existing Inventory: ");
+        console.log("...................\n");
+
+        var storeOut = "";
+        for (var i = 0; i < data.length; i++) {
+            storeOut = "";
+            storeOut += "Item ID: " + data[i].item_id + "  //  ";
+            storeOut += "Product Name: " + data[i].product_name + "  //  ";
+            storeOut += "Department: " + data[i].department_name + "  //  ";
+            storeOut += "Price: $" + data[i].price + "  //  ";
+            storeOut += "Quantity: " + data[i].stock_quantity + "\n";
+
+            console.log(storeOut);
+        }
+        console.log("---------------------------------------------------------------------\n");
+
+        connection.end();
+    })
+}
+
+function displayLowInventory() {
+    queryStore = "SELECT * FROM products WHERE stock_quantity < 100";
+    connection.query(queryStore, function (err, data) {
+        if (err) throw err;
+
+        console.log("Low Inventory Items (below 10): ");
+        console.log("................................\n");
+
+        var storeOut = "";
+        for (var i = 0; i < data.length; i++) {
+            storeOut = "";
+            storeOut += "Item ID: " + data[i].item_id + "  //  ";
+            storeOut += "Product Name: " + data[i].product_name + "  //  ";
+            storeOut += "Department: " + data[i].department_name + "  //  ";
+            storeOut += "Price: $" + data[i].price + "  //  ";
+            storeOut += "Quantity: " + data[i].stock_quantity + "\n";
+
+            console.log(storeOut);
+        }
+
+        console.log("---------------------------------------------------------------------\n");
+
+        connection.end();
+    })
+}
+
+function validateInteger(value) {
     var integer = Number.isInteger(parseFloat(value));
     var sign = Math.sign(value);
 
@@ -23,97 +110,115 @@ function validateInput(value) {
     }
 }
 
-function promptUserPurchase() {
-    inquirer.prompt([{
+function validateNumeric(value) {
+        var number = (typeof parseFloat(value)) === "number";
+        var positive = parseFloat(value) > 0;
+
+        if (number && positive) {
+            return true;
+        } else {
+            return "Please enter a positive number for the unit price."
+        }
+    }
+
+    function addInventory() {
+        inquirer.prompt([{
                 type: "input",
                 name: "item_id",
-                message: "Please enter the Item ID for the item you would like to purchase.",
-                validate: validateInput,
+                message: "Please enter the Item ID for stock_count update.",
+                validate: validateInteger,
                 filter: Number
             },
             {
                 type: "input",
                 name: "quantity",
-                message: "How many do you want?",
-                validate: validateInput,
+                message: "How many would you like to add?",
+                validate: validateInteger,
                 filter: Number
             }
         ]).then(function (input) {
-                var item = input.item_id;
-                var quantity = input.quantity;
 
-                var queryStore = "SELECT * FROM products WHERE ?";
-            
-                connection.query(queryStore, {
-                            item_id: item
-                        }, function (err, data) {
-                            if (err) throw err;
+            var item = input.item_id;
+            var addQuantity = input.quantity;
 
-                            if (data.length === 0) {
-                                console.log("ERROR: No such Item ID. Please select a valid Item ID.");
-                                displayInventory();
+            var queryStore = 'SELECT * FROM products WHERE ?';
 
-                            } else {
-                                var productData = data[0];
+            connection.query(queryStore, {
+                item_id: item
+            }, function (err, data) {
+                if (err) throw err;
 
-                                if (quantity <= productData.stock_quantity) {
-                                    console.log("Yay! The product you requested is in stock! Placing order!");
+                if (data.length === 0) {
+                    console.log("ERROR: No such Item ID. Please select a valid Item ID.");
+                    addInventory();
 
-                                    var updateQueryStore = "UPDATE products SET stock_quantity = " + (productData.stock_quantity - quantity) + " WHERE item_id = " + item;
+                } else {
+                    var productData = data[0];
 
-                                    connection.query(updateQueryStore, function (err, data) {
-                                        if (err) throw err;
+                    console.log("Updating Inventory...");
 
-                                        console.log("Your order has been placed! Your total is $" + productData.price * quantity);
-                                        console.log("Thank you for shopping with us!");
-                                        console.log("\n---------------------------------------------------------------------\n");
+                    var updateQueryStore = "UPDATE products SET stock_quantity = " + (productData.stock_quantity + addQuantity) + " WHERE item_id = " + item;
 
-                                        connection.end();
-                                    })
+                    connection.query(updateQueryStore, function (err, data) {
+                        if (err) throw err;
 
-                                        } else {
-                                        console.log("Sorry, there is not enough product in stock, your order can not be placed as is.");
-                                        console.log("Please modify your order.");
-                                        console.log("\n---------------------------------------------------------------------\n");
+                        console.log("Stock count for Item ID " + item + " has been updated to " + (productData.stock_quantity + addQuantity) + ".");
+                        console.log("\n---------------------------------------------------------------------\n");
 
-                                        displayInventory();
-                                    }
-                                }
-                            })
-                        })
-                    }
-                    function displayInventory() {
-
-                        queryStore = "SELECT * FROM products";
-
-                        connection.query(queryStore, function(err, data) {
-                            if (err) throw err;
-
-                            console.log("Existing Inventory: ");
-                            console.log("...................\n");
-                            
-                            var storeOut = "";
-                            for (var i = 0; i < data.length; i++) {
-                                storeOut = "";
-                                storeOut += "Item ID: " + data[i].item_id + "  //  ";
-                                storeOut += "Product Name: " + data[i].product_name + "  //  ";
-                                storeOut += "Department: " + data[i].department_name + "  //  ";
-                                storeOut += "Price: $" + data[i].price + "\n";
-                    
-                                console.log(storeOut);
-                            }
-
-                            console.log("---------------------------------------------------------------------\n");
-                  
-                            
-                            promptUserPurchase();
-                      })
-                  }
-                  function runBamazon() {
-                    
-                    displayInventory();
-
+                        connection.end();
+                    })
                 }
+            })
+        })
+    }
 
-            
-                runBamazon();
+    function createNewProduct() {
+        inquirer.prompt([{
+                type: "input",
+                name: "product_name",
+                message: "Please enter the new product name.",
+            },
+            {
+                type: "input",
+                name: "department_name",
+                message: "Which department does the new product belong to?",
+            },
+            {
+                type: "input",
+                name: "price",
+                message: "What is the price per unit?",
+                validate: validateNumeric
+            },
+            {
+                type: "input",
+                name: "stock_quantity",
+                message: "How many items are in stock?",
+                validate: validateInteger
+            }
+        ]).then(function (input) {
+
+            console.log("Adding New Item: \n    product_name = " + input.product_name + "\n" +
+                "    department_name = " + input.department_name + "\n" +
+                "    price = " + input.price + "\n" +
+                "    stock_quantity = " + input.stock_quantity);
+
+            var queryStore = "INSERT INTO products SET ?";
+
+            connection.query(queryStore, input, function (error, results, fields) {
+                if (error) throw error;
+
+                console.log("New product has been added to the inventory under Item ID " + results.insertId + ".");
+                console.log("\n---------------------------------------------------------------------\n");
+
+                connection.end();
+            });
+        })
+    }
+
+    function runBamazon() {
+
+        promptManagerAction();
+    }
+    runBamazon(); {
+
+    }
